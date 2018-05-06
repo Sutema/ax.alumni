ef04fe2c9833dbd59a2d511cbc0c2fedecb792e4
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2018-05-06 17:18:11 +0700

    WIP: login flow

--- app/src/main/java/com/sutema/apps/alumnitracker/view/LoginActivity.java
+++ app/src/main/java/com/sutema/apps/alumnitracker/view/LoginActivity.java
@@ -5,6 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.sutema.apps.alumnitracker.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -18,5 +24,25 @@ public class LoginActivity extends AppCompatActivity {
     public void openSignUp(View view){
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
+    }
+
+    public void tryLogin(View view){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "http://192.168.1.100/be.alumni/index.php/api/users/";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Didnt work");
+            }
+        });
+
+        requestQueue.add(stringRequest);
+
     }
 }
